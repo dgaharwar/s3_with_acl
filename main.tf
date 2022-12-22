81b3f5ead6ddd066ab6f1e610d9e0dc147471019
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-12-22 17:05:48 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_s3_bucket" "bucket" {
       }
 }
 resource "aws_s3_bucket_acl" "acl" {
-    bucket = aws_s3_bucket.bucket.id
+    bucket = "${aws_s3_bucket.bucket.id}"
     acl    = "private"
 }
